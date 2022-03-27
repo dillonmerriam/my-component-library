99e99177236ae3b710ebc32d29b7ab0f78059a76
dillonmerriam <merriamdillon@yahoo.com>
2022-03-26 23:19:15 -0700

    styled buttons, modals

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle, defaultTheme, darkTheme } from "./utils";
+import { SignUpModal, PrimaryButton } from "./components";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [useDarkTheme, setUseDarkTheme] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(true)}
+      >
+        Dark theme
+      </button>
+      <button
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(false)}
+      >
+        Default theme
+      </button>
+      <PrimaryButton
+        style={{ margin: "0 16px" }}
+        onClick={() => setShowModal(!showModal)}
+      >
+        Show modal
+      </PrimaryButton>
+      <div
+        style={{
+          background: useDarkTheme
+            ? defaultTheme.primaryColor
+            : darkTheme.primaryColor,
+          width: "100vw",
+          height: "90vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around"
+        }}
+      >
+        <SignUpModal showModal={showModal} setShowModal={setShowModal} />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        <GlobalStyle />
+      </div>
+    </ThemeProvider>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
