9e2e5039ecf1a9edd02cba2731f96df79d0fd9e6
dillonmerriam <merriamdillon@yahoo.com>
2022-04-02 00:38:56 -0700

    adding decorator and context, currently renders blank page

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,8 @@ module.exports = {
     "@storybook/addon-actions",
     "@storybook/addon-links",
     "@storybook/addon-knobs",
-    
+    "@storybook/addon-backgrounds/register",
+    "@storybook/addon-contexts/register"
 
 
   ]
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,12 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+import { addParameters, addDecorator } from "@storybook/react";
+import { withContexts } from "@storybook/addon-contexts/react";
+import { contexts } from "./contexts";
+
+addParameters({
+  backgrounds: [
+    { name: "Default theme", value: "#ffffff", default: true },
+    { name: "Dark theme", value: "#233e31" }
+  ]
+});
+
+addDecorator(withContexts(contexts));
