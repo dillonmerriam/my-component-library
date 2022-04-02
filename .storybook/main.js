6fbf8d024cd54f8ccfa4c3d57cd1e4ae2da1f857
dillonmerriam <merriamdillon@yahoo.com>
2022-04-01 23:33:36 -0700

    implementing storybook

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+module.exports = {
+  core: { builder: 'webpack5', }, 
+  presets: ['@storybook/addon-docs/preset'],
+  stories: ['../src/**/*.stories.(js|mdx)'],
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-knobs",
+    "@storybook/addon-docs"
+
+
+  ]
+};
